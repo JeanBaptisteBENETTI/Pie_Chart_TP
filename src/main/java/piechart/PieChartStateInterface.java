9b2a9beebc93bd836a825cf9f61adb84bf3bc57a
JeanBaptisteBENETTI <jean-baptiste.benetti@etud.univ-jfc.fr>
2024-04-04 12:06:51 +0200

    Add PieChartStateInterface.java

---
+++ src/main/java/piechart/PieChartStateInterface.java
@@ -0,0 +1,10 @@
+package piechart;
+
+import java.awt.event.MouseEvent;
+
+public interface PieChartStateInterface {
+    void mousePressed(MouseEvent e, PercentagePieChart chart);
+    void mouseReleased(MouseEvent e, PercentagePieChart chart);
+    void mouseDragged(MouseEvent e, PercentagePieChart chart);
+    void mouseMoved(MouseEvent e, PercentagePieChart chart); 
+}
